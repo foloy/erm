e3312f31e4dfff47bd2f6f879fe81826916bb7f2
Fiery Tsui <programmer_y1@163.com>
2015-03-11 16:42:25 +0800

    15/3/11 16:42

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-
-/*
- * GET home page.
+/**
+ * Created by Administrator on 2015/3/11.
  */
-
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-  /*master*/
-};
+module.exports=function(app){
+    app.get('/', function (req, res) {
+        res.render('index',{title:'index' })
+    })
+    app.get('/new',function(req,res){
+        res.render('index',{title:'new'})
+    })
+}
